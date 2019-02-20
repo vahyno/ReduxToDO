d5dad5912e3dff08474548fc80b82e07fdf287c3
vahyno <vahyno@gmail.com>
2019-02-19 17:03:47 -0800

    refactor to switch statement

--- index.js
+++ index.js
@@ -31,15 +31,16 @@ function createStore (reducer) {
 //App Code
 // reducer 
 function todos (state = [], action) {
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo]);
-    } else if (action.type === 'REMOVE_TODO') {
-        return state.filter((todo)=> todo.id !== action.id);
-    } else if (action.type === 'TOGGLE_TODO') {
-        return state.map((todo)=> todo.id !== action.id ? todo :
-            Object.assign({}, todo, {complete: !todo.complete}));
-    } else {
-        return state;
+    switch (action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo]);
+        case 'REMOVE_TODO':
+            return state.filter((todo)=> todo.id !== action.id);
+        case 'TOGGLE_TODO':
+            return state.map((todo)=> todo.id !== action.id ? todo :
+                Object.assign({}, todo, {complete: !todo.complete}));
+        default:
+            return state;
     }
 }
 
